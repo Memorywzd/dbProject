976232ffc0bc58b64931f8e937eba545d45cba88
Memorywzd <1746607977@Qq.com>
2023-01-16 16:01:31 +0800

    更新登录逻辑

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,8 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 const cache = window.localStorage;
-const serverURL = "http://localhost:8080";
+const serverURL = "http://az.pizzel.me";
+//const serverURL = "http://localhost:8080";
 
 function Home() {
     //定义页面跳转函数
@@ -17,20 +18,55 @@ function Home() {
     const [isTeacherLogin, setIsTeacherLogin] = useState(false);
     const [isMentorLogin, setIsMentorLogin] = useState(false);
     const [isStudentLogin, setIsStudentLogin] = useState(false);
-    const [userRole, setUserRole] = useState("0");
+    const [userRole, setUserRole] = useState(0);
     const [token, setToken] = useState("");
+
+    function  setLoginState(role) {
+        console.log("role: " + role);
+        switch (role) {
+            case 1:
+            case '1':
+                console.log("研究生登录！");
+                setIsStudentLogin(true);
+                break;
+            case 2:
+            case '2':
+                console.log("教师登录！");
+                setIsTeacherLogin(true);
+                break;
+            case 3:
+            case '3':
+                console.log("导师登录！");
+                setIsMentorLogin(true);
+                break;
+            case 4:
+            case '4':
+                console.log("管理员登录！");
+                setIsAdminLogin(true);
+                break;
+            case 5:
+            case '5':
+                console.log("学科负责人登录！");
+                setIsLeaderLogin(true);
+                break;
+            default:
+                break;
+        }
+    }
 
     useEffect(() => {
         //判断是否登录
         if (cache.getItem("token") && cache.getItem("role")) {
-            console.log("已经登录");
-            setToken(cache.getItem("token"));
+            console.log("已经登录,token:" + cache.getItem("token") +
+                ",role:" + cache.getItem("role"));
+            setLoginState(cache.getItem("role"));
         }
     }, []);
+
     //登录函数
     function doLogin() {
         const formData = new FormData();
-        formData.append("username", username);
+        formData.append("userID", username);
         formData.append("password", password);
         formData.append("role", userRole);
 
@@ -39,14 +75,19 @@ function Home() {
             .then((res) => {
                 console.log(res);
                 if (res.status === 200) {
-                    console.log("登录成功");
-                    console.log(res.data);
                     setToken(res.data);
-                    cache.setItem("token", token);
+                    setLoginState(userRole);
+                    cache.setItem("token", res.data);
                     cache.setItem("role", userRole);
+                    console.log("登录成功, token:" + cache.getItem("token"));
+                    console.log("role为" + cache.getItem("role"));
                 }
             })
 
+    }
+    //清空缓存
+    function clearCache() {
+        cache.clear();
     }
 
     function afterAdminLogin() {
@@ -157,6 +198,7 @@ function Home() {
     return (
         <div className="App">
             <h1>研究生培养环节和成果认定综合管理系统</h1>
+            <p><button onClick={clearCache}>清除系统缓存</button></p>
             <label>用户角色：
                 <select onChange={(e) => {
                     setUserRole(e.target.selectedIndex);
